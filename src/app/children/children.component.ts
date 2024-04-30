bd0a03bd47036423ab042a0e5cbbbb7ac3cafd5f
JuanSCaicedo <sebanchit@gmail.com>
2024-04-30 14:58:05 -0500

    @Output (comunicación entre componentes)

--- src/app/children/children.component.ts
+++ src/app/children/children.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-children',
@@ -6,5 +6,12 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./children.component.css']
 })
 export class ChildrenComponent {
+  
   @Input() title?: string;
+
+  @Output() titleChange = new EventEmitter<string>();
+
+  emitTitleChange() {
+    this.titleChange.emit(this.title);
+  }
 }
