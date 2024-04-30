4afaf058715bbd1d045c5fa35c9f409cb9ca5903
JuanSCaicedo <sebanchit@gmail.com>
2024-04-30 08:45:45 -0500

    formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SwitchComponent } from './switch/switch.component';
 import { Formulario2Component } from './formulario2/formulario2.component';
 
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Formulario3Component } from './formulario3/formulario3.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +21,13 @@ import { FormsModule } from '@angular/forms';
     FormularioComponent,
     BuclesComponent,
     SwitchComponent,
-    Formulario2Component
+    Formulario2Component,
+    Formulario3Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
